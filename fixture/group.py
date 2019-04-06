850c6997fc23c483cde12f8a6f0185830bffb505
widoc84 <sysadmin84@gmail.com>
2019-04-06 13:36:26 +0400

    Задание 24 Генерация и чтение из Excel

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,7 @@
+from comtypes.client import CreateObject
+import os
+import random
+
 class GH:
     def __init__(self, app):
         self.app = app
@@ -38,3 +42,16 @@ class GH:
 
     def close_group_editor(self):
         self.group_editor.close()
+
+    def read_excel(self):
+        project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        xl = CreateObject("Excel.application")
+        xl.Workbooks.Open(os.path.join(project_dir,"groups.xlsx"))
+        xl.Visible = True
+        sheet = xl.Workbooks[1].Sheets["Лист1"]
+        list = []
+        for i in sheet.Range("A1", "A10"):
+            list.append(i.Value())
+        item = random.choice(list)
+        xl.Quit()
+        return item
--- test/test_group.py
+++ test/test_group.py
@@ -1,9 +1,11 @@
 
+
 def test_add_group(app):
     old_list = app.groups.get_group_list()
-    app.groups.add_new_group("new_group")
+    item = app.groups.read_excel()
+    app.groups.add_new_group(item)
     new_list = app.groups.get_group_list()
-    old_list.append("new_group")
+    old_list.append(item)
     assert  sorted(old_list) == sorted(new_list)
 
 
